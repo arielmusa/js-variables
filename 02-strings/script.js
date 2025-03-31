284b6153bdf0b28fadee2fe05f63f13cc32a9e82
arielmusa <arielmusainfo@gmail.com>
2025-03-31 16:00:13 +0200

    02-strings.js: second excercize

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,15 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+const number = 42;
+console.log(number);
+
+const text = "solution";
+console.log(text);
+
+const bool = true;
+console.log(bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente
 // in console la stringa con il nome completo
